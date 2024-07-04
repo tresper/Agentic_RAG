eadcd184fc597a789b6c88be64a4b2afc9b7ba65
tresper <dbailley@umich.edu>
2024-07-04 16:36:25 +0300

    changed port to 5433

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     database_host: str = os.getenv("DATABASE_HOST", "localhost")
-    connection_str: str = f"postgresql://postgres:postgres@{database_host}:5432/vector_db"
+    connection_str: str = f"postgresql://postgres:postgres@{database_host}:5433/vector_db"
     index_table: str = "ajua_demo_hybrid_search"
     embed_dim: int = 3072
 
